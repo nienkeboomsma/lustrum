9ee0051e78786eb8e60b3069cc8c2f06e6c8ec4f
Nienke Boomsma <12957143+nienkeboomsma@users.noreply.github.com>
2023-12-09 20:29:29 +0100

    feat: add Button component (#5)
    * add Button component
    
    * refactor: use css helper

---
+++ src/components/Forms/Button.tsx
@@ -0,0 +1,164 @@
+import styled, { css } from 'styled-components'
+import { DefaultTheme } from 'styled-components'
+
+enum Intent {
+  Primary = 'primary',
+  Secondary = 'secondary',
+}
+
+enum Shape {
+  Rectangle = 'rectangle',
+  Lozenge = 'lozenge',
+}
+
+enum Size {
+  Medium = 'medium',
+  Large = 'large',
+}
+
+interface StyleTypes {
+  $intent: Intent
+  $shadow: boolean
+  $shape: Shape
+  $size: Size
+}
+
+const getIntentStyles = ({
+  theme,
+  $intent,
+}: {
+  theme: DefaultTheme
+  $intent: Intent
+}) => {
+  switch ($intent) {
+    case Intent.Primary:
+      return {
+        default: {
+          backgroundColor: theme.s500,
+          borderColor: theme.s500,
+          color: theme.s50,
+        },
+        hover: {
+          backgroundColor: theme.s400,
+          borderColor: theme.s400,
+          color: theme.s50,
+        },
+      }
+    case Intent.Secondary:
+      return {
+        default: {
+          backgroundColor: 'white',
+          borderColor: theme.s500,
+          color: theme.s500,
+        },
+        hover: {
+          backgroundColor: theme.s50,
+          borderColor: theme.s500,
+          color: theme.s500,
+        },
+      }
+  }
+}
+
+const getBoxShadow = (
+  {
+    theme,
+    $shadow,
+  }: {
+    theme: DefaultTheme
+    $shadow: boolean
+  },
+  active: boolean
+) => {
+  if (!$shadow) return
+
+  const defaultShadow = `0 16px 20px -4px ${theme.s200},
+  0 6px 8px -5px ${theme.s200};`
+  const activeShadow = `0 10px 15px -3px ${theme.s200},
+  0 4px 6px -4px ${theme.s200}`
+
+  return active ? activeShadow : defaultShadow
+}
+
+const getBorderRadius = ({ $shape }: { $shape: Shape }) => {
+  switch ($shape) {
+    case Shape.Rectangle:
+      return '0.25rem'
+    case Shape.Lozenge:
+      return '1.5rem'
+  }
+}
+
+const getSizeStyles = ({ $size }: { $size: Size }) => {
+  switch ($size) {
+    case Size.Medium:
+      return {
+        fontSize: '0.75rem',
+        letterSpacing: '0.0875rem',
+        padding: '0.375rem 0.5rem',
+      }
+    case Size.Large:
+      return {
+        fontSize: '0.875rem',
+        letterSpacing: '0.0625rem',
+        padding: '0.5rem 0.75rem',
+      }
+  }
+}
+
+const StyledButton = styled.button<StyleTypes>`
+  ${(props) => css`
+    background-color: ${getIntentStyles(props).default.backgroundColor};
+    border-color: ${getIntentStyles(props).default.borderColor};
+    border-radius: ${getBorderRadius(props)};
+    border-style: solid;
+    border-width: 1px;
+    box-shadow: ${getBoxShadow(props, false)};
+    color: ${getIntentStyles(props).default.color};
+    font-family: inherit;
+    font-size: ${getSizeStyles(props).fontSize};
+    font-weight: 300;
+    letter-spacing: ${getSizeStyles(props).letterSpacing};
+    padding: ${getSizeStyles(props).padding};
+    text-align: center;
+    text-transform: uppercase;
+    user-select: none;
+
+    &:active {
+      box-shadow: ${getBoxShadow(props, false)};
+      transform: translateY(1px);
+      transition: transform cubic-bezier(0.4, 0, 0.2, 1) 150ms;
+    }
+
+    &:hover {
+      background-color: ${getIntentStyles(props).hover.backgroundColor};
+      border-color: ${getIntentStyles(props).hover.borderColor};
+      color: ${getIntentStyles(props).hover.color};
+      cursor: pointer;
+    }
+  `}
+`
+
+export default function Button({
+  children,
+  intent,
+  shadow,
+  shape,
+  size,
+}: {
+  children: React.ReactNode
+  intent: Intent
+  shadow: boolean
+  shape: Shape
+  size: Size
+}) {
+  return (
+    <StyledButton $intent={intent} $shape={shape} $shadow={shadow} $size={size}>
+      {children}
+    </StyledButton>
+  )
+}
+
+Button.Intent = Intent
+Button.Shape = Shape
+Button.Size = Size
--- src/components/Posts/PostDate.tsx
+++ src/components/Posts/PostDate.tsx
@@ -2,31 +2,11 @@
 
 import styled from 'styled-components'
 import Link from 'next/link'
+import Button from '../Forms/Button'
 
 const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.s500};
-  border-radius: 1.5rem;
-  box-shadow: 0 16px 20px -4px ${({ theme }) => theme.s200},
-    0 6px 8px -5px ${({ theme }) => theme.s200};
-  color: ${({ theme }) => theme.s50};
-  font-size: 0.875rem;
-  font-weight: 300;
-  letter-spacing: 0.0625rem;
   margin: 1.5rem auto;
   max-width: fit-content;
-  padding: 0.375rem 0.875rem;
-  text-transform: uppercase;
-
-  &:hover {
-    background-color: ${({ theme }) => theme.s400};
-  }
-
-  &:active {
-    transform: translateY(1px);
-    transition: transform cubic-bezier(0.4, 0, 0.2, 1) 150ms;
-    box-shadow: 0 10px 15px -3px ${({ theme }) => theme.s200},
-      0 4px 6px -4px ${({ theme }) => theme.s200};
-  }
 `
 
 export default function PostDate({
@@ -53,7 +33,16 @@ export default function PostDate({
 
   return (
     <Link href={href}>
-      <Wrapper>{displayDate}</Wrapper>
+      <Wrapper>
+        <Button
+          intent={Button.Intent.Primary}
+          shadow={true}
+          shape={Button.Shape.Lozenge}
+          size={Button.Size.Large}
+        >
+          {displayDate}
+        </Button>
+      </Wrapper>
     </Link>
   )
 }
