d21777949578b57be12272dcf8838a1a6c695e0a
Nienke Boomsma <12957143+nienkeboomsma@users.noreply.github.com>
2024-01-18 10:01:14 +0100

    feat: restore focus to last element on modal close

--- src/hooks/useModal.tsx
+++ src/hooks/useModal.tsx
@@ -1,10 +1,23 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 export default function useModal() {
   const [visible, setVisible] = useState(false)
+  const lastElement = useRef<HTMLElement | null>(
+    document ? document.body : null
+  )
 
-  const openModal = () => setVisible(true)
-  const closeModal = () => setVisible(false)
+  const openModal = () => {
+    lastElement.current =
+      document.activeElement instanceof HTMLElement
+        ? document.activeElement
+        : document.body
+    setVisible(true)
+  }
+
+  const closeModal = () => {
+    setVisible(false)
+    if (lastElement.current) lastElement.current.focus()
+  }
 
   return { visible, openModal, closeModal }
 }
